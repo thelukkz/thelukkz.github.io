f246e5819faee2b284136aca62c7163fc5e0e32f
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-08-03 20:29:34 +0200

    feat: cards with projects (wip)

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,30 @@
 
 // place your code below
 
+const list = document.querySelector('.projects__list--js');
+
+fetch("https://api.github.com/users/thelukkz/repos")
+.then(resp => resp.json())
+.then(resp => {
+    const dtos = resp;
+    for (const model of dtos){
+        const {name, html_url, homepage, description} = model;
+        list.innerHTML += `<li class="projects__item">
+        <div class="proj-item__info">
+          <img class="proj-item__logo" src="../assets/img/github.svg" alt="Repository provider, Github logo."/>
+          <h4 class="proj-item__title">${name}</h4>
+          <p class="proj-item__description">${description}</p>
+        </div>
+        <div class="proj-item__navi">
+          <a class="proj-item__link proj-item__link--page" href="${homepage}" target="_blank">Demo</a>
+          <a class="proj-item__link proj-item__link--repo" href="${html_url}" target="_blank">GitHub</a>
+        </div>
+      </li>`;
+    }
+})
+.catch(err => {
+    console.log("Can't connect to github api.")
+})
 
 console.log(`Hello world!`);
 
